431f42bfadaa74ff4f176a6b05681fbf895738e0
Alexandr Shapkin <shapkin.alexandr.sergeevich@gmail.com>
2024-12-07 14:04:12 +0700

    test:more cli test

--- cli_test.go
+++ cli_test.go
@@ -81,6 +81,159 @@ func Test_cli_OneCmd(t *testing.T) {
 			},
 		},
 		&Command{
+			Use: "withlongargflag",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag",
+					Long: "flag",
+					Short: "f",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := "flag"
+				wantName := "flag"
+				wantFlagArg := "flagarg"
+				flag, ok := flags[wantType]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType)
+					return
+				}
+				if strings.Compare(flag.Type, wantType) != 0 {
+					t.Errorf("flag type is incorrect want %s, got %s", wantType, flag.Type)
+					return
+				}
+				if strings.Compare(flag.Name, wantName) != 0 {
+					t.Errorf("flag name is incorrect want %s, got %s", wantName, flag.Name)
+					return
+				}
+				if strings.Compare(flag.Args, wantFlagArg) != 0 {
+					t.Errorf("flag arg is incorrect want %s, got %s", wantFlagArg, flag.Name)
+					return
+				}
+			},
+		},
+		&Command{
+			Use: "withlongflags",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag0",
+					Long: "flag0",
+					Short: "f",
+				},
+				{
+					Type: "flag1",
+					Long: "flag1",
+					Short: "a",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := []string{"flag0", "flag1"}
+				wantName := []string{"flag0", "flag1"}
+				flag0, ok := flags[wantType[0]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[0])
+					return
+				}
+				flag1, ok := flags[wantType[1]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[1])
+					return
+				}
+				if strings.Compare(flag0.Type, wantType[0]) != 0 || strings.Compare(flag1.Type, wantType[1]) != 0 {
+					t.Errorf("flag type is incorrect want %v, got %v", wantType, []string{flag0.Type, flag1.Type})
+					return
+				}
+				if strings.Compare(flag0.Name, wantName[0]) != 0 || strings.Compare(flag1.Name, wantName[1]) != 0 {
+					t.Errorf("flag name is incorrect want %v, got %v", wantType, []string{flag0.Name, flag1.Name})
+					return
+				}
+			},
+		},
+		&Command{
+			Use: "withlongflagswithargs",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag0",
+					Long: "flag0",
+					Short: "f",
+				},
+				{
+					Type: "flag1",
+					Long: "flag1",
+					Short: "a",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := []string{"flag0", "flag1"}
+				wantName := []string{"flag0", "flag1"}
+				wantFlagArg := []string{"flagarg0", "flagarg1"}
+				flag0, ok := flags[wantType[0]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[0])
+					return
+				}
+				flag1, ok := flags[wantType[1]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[1])
+					return
+				}
+				if strings.Compare(flag0.Type, wantType[0]) != 0 || strings.Compare(flag1.Type, wantType[1]) != 0 {
+					t.Errorf("flag type is incorrect want %v, got %v", wantType, []string{flag0.Type, flag1.Type})
+					return
+				}
+				if strings.Compare(flag0.Name, wantName[0]) != 0 || strings.Compare(flag1.Name, wantName[1]) != 0 {
+					t.Errorf("flag name is incorrect want %v, got %v", wantType, []string{flag0.Name, flag1.Name})
+					return
+				}
+				if strings.Compare(flag0.Args, wantFlagArg[0]) != 0 || strings.Compare(flag1.Args, wantFlagArg[1]) != 0 {
+					t.Errorf("flag args is incorrect want %v, got %v", wantFlagArg, []string{flag0.Args, flag1.Args})
+					return
+				}
+			},
+		},
+		&Command{
+			Use: "withlongflagswithmargs",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag0",
+					Long: "flag0",
+					Short: "f",
+				},
+				{
+					Type: "flag1",
+					Long: "flag1",
+					Short: "a",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := []string{"flag0", "flag1"}
+				wantName := []string{"flag0", "flag1"}
+				wantFlagArg := []string{"flagarg0 flagarg1", "flagarg2 flagarg3"}
+				flag0, ok := flags[wantType[0]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[0])
+					return
+				}
+				flag1, ok := flags[wantType[1]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[1])
+					return
+				}
+				if strings.Compare(flag0.Type, wantType[0]) != 0 || strings.Compare(flag1.Type, wantType[1]) != 0 {
+					t.Errorf("flag type is incorrect want %v, got %v", wantType, []string{flag0.Type, flag1.Type})
+					return
+				}
+				if strings.Compare(flag0.Name, wantName[0]) != 0 || strings.Compare(flag1.Name, wantName[1]) != 0 {
+					t.Errorf("flag name is incorrect want %v, got %v", wantType, []string{flag0.Name, flag1.Name})
+					return
+				}
+				if strings.Compare(flag0.Args, wantFlagArg[0]) != 0 || strings.Compare(flag1.Args, wantFlagArg[1]) != 0 {
+					t.Errorf("flag args is incorrect want %v, got %v", wantFlagArg, []string{flag0.Args, flag1.Args})
+					return
+				}
+			},
+		},
+		&Command{
 			Use: "withshortflag",
 			Flags: []*CommandFlag {
 				{
@@ -103,6 +256,80 @@ func Test_cli_OneCmd(t *testing.T) {
 				}
 				if strings.Compare(flag.Name, wantName) != 0 {
 					t.Errorf("flag name is incorrect want %s, got %s", wantName, flag.Name)
+					return
+				}
+			},
+		},
+		&Command{
+			Use: "withshortflags",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag0",
+					Long: "flag0",
+					Short: "f",
+				},
+				{
+					Type: "flag1",
+					Long: "flag1",
+					Short: "a",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := []string{"flag0", "flag1"}
+				wantName := []string{"f", "a"}
+				flag0, ok := flags[wantType[0]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[0])
+					return
+				}
+				flag1, ok := flags[wantType[1]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[1])
+					return
+				}
+				if strings.Compare(flag0.Type, wantType[0]) != 0 || strings.Compare(flag1.Type, wantType[1]) != 0 {
+					t.Errorf("flag type is incorrect want %v, got %v", wantType, []string{flag0.Type, flag1.Type})
+					return
+				}
+				if strings.Compare(flag0.Name, wantName[0]) != 0 || strings.Compare(flag1.Name, wantName[1]) != 0 {
+					t.Errorf("flag name is incorrect want %v, got %v", wantType, []string{flag0.Name, flag1.Name})
+					return
+				}
+			},
+		},
+				&Command{
+			Use: "withshortflags",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag0",
+					Long: "flag0",
+					Short: "f",
+				},
+				{
+					Type: "flag1",
+					Long: "flag1",
+					Short: "a",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := []string{"flag0", "flag1"}
+				wantName := []string{"f", "a"}
+				flag0, ok := flags[wantType[0]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[0])
+					return
+				}
+				flag1, ok := flags[wantType[1]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[1])
+					return
+				}
+				if strings.Compare(flag0.Type, wantType[0]) != 0 || strings.Compare(flag1.Type, wantType[1]) != 0 {
+					t.Errorf("flag type is incorrect want %v, got %v", wantType, []string{flag0.Type, flag1.Type})
+					return
+				}
+				if strings.Compare(flag0.Name, wantName[0]) != 0 || strings.Compare(flag1.Name, wantName[1]) != 0 {
+					t.Errorf("flag name is incorrect want %v, got %v", wantType, []string{flag0.Name, flag1.Name})
 					return
 				}
 			},
@@ -164,6 +391,114 @@ func Test_cli_OneCmd(t *testing.T) {
 				}
 				if len(args) != 1 || args[0] != "arg" {
 					t.Errorf("argument is incorrect")
+				}
+			},
+		},
+		&Command{
+			Use: "withlongflagandmultiwordargs",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag",
+					Long: "flag",
+					Short: "f",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := "flag"
+				wantName := "flag"
+				wantArg := []string{"arg0 arg1", "arg2 arg3"}
+				flag, ok := flags[wantType]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType)
+					return
+				}
+				if strings.Compare(flag.Type, wantType) != 0 {
+					t.Errorf("flag type is incorrect want %s, got %s", wantType, flag.Type)
+					return
+				}
+				if strings.Compare(flag.Name, wantName) != 0 {
+					t.Errorf("flag name is incorrect want %s, got %s", wantName, flag.Name)
+					return
+				}
+				if len(args) != 2 || args[0] != wantArg[0] || args[1] != wantArg[1] {
+					t.Errorf("argument is incorrect want = %v, got = %v", wantArg, args)
+				}
+			},
+		},
+		&Command{
+			Use: "withshortflagandmultiwordargs",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag",
+					Long: "flag",
+					Short: "f",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := "flag"
+				wantName := "f"
+				wantArg := []string{"arg0 arg1", "arg2 arg3"}
+				flag, ok := flags[wantType]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType)
+					return
+				}
+				if strings.Compare(flag.Type, wantType) != 0 {
+					t.Errorf("flag type is incorrect want %s, got %s", wantType, flag.Type)
+					return
+				}
+				if strings.Compare(flag.Name, wantName) != 0 {
+					t.Errorf("flag name is incorrect want %s, got %s", wantName, flag.Name)
+					return
+				}
+				if len(args) != 2 || args[0] != wantArg[0] || args[1] != wantArg[1] {
+					t.Errorf("argument is incorrect want = %v, got = %v", wantArg, args)
+				}
+			},
+		},
+		&Command{
+			Use: "wlfwmaama",
+			Flags: []*CommandFlag {
+				{
+					Type: "flag0",
+					Long: "flag0",
+					Short: "f",
+				},
+				{
+					Type: "flag1",
+					Long: "flag1",
+					Short: "a",
+				},
+			},
+			Run: func(flags map[string]*ParsedCommandFlags, args []string) {
+				wantType := []string{"flag0", "flag1"}
+				wantName := []string{"flag0", "flag1"}
+				wantArgs := []string{"arg0 arg1", "arg2 arg3"}
+				wantFlagArg := []string{"flagarg0 flagarg1", "flagarg2 flagarg3"}
+				flag0, ok := flags[wantType[0]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[0])
+					return
+				}
+				flag1, ok := flags[wantType[1]]
+				if !ok {
+					t.Errorf("flag with type %s not found", wantType[1])
+					return
+				}
+				if strings.Compare(flag0.Type, wantType[0]) != 0 || strings.Compare(flag1.Type, wantType[1]) != 0 {
+					t.Errorf("flag type is incorrect want %v, got %s", wantType, []string{flag0.Type, flag1.Type})
+					return
+				}
+				if strings.Compare(flag0.Name, wantName[0]) != 0 || strings.Compare(flag1.Name, wantName[1]) != 0 {
+					t.Errorf("flag name is incorrect want %v, got %v", wantName, []string{flag0.Name, flag1.Name})
+					return
+				}
+				if len(args) != 2 || args[0] != wantArgs[0] || args[1] != wantArgs[1] {
+					t.Errorf("argument is incorrect want = %v, got = %v", wantArgs, args)
+				}
+				if strings.Compare(flag0.Args, wantFlagArg[0]) != 0 || strings.Compare(flag1.Args, wantFlagArg[1]) != 0 {
+					t.Errorf("flag args is incorrect want %v, got %v", wantFlagArg, []string{flag0.Args, flag1.Args})
+					return
 				}
 			},
 		},
@@ -234,10 +569,58 @@ func Test_cli_OneCmd(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "cmd with long flag",
+			c: cli,
+			args: args {
+				input: "withlongargflag --flag=flagarg",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with long flags",
+			c: cli,
+			args: args {
+				input: "withlongflags --flag0 --flag1",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with long flags with args",
+			c: cli,
+			args: args {
+				input: "withlongflags --flag0=flagarg0 --flag1=flagarg1",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with long flags with multiword args",
+			c: cli,
+			args: args {
+				input: "withlongflagswithmargs --flag0=\"flagarg0 flagarg1\" --flag1='flagarg2 flagarg3'",
+			},
+			wantErr: false,
+		},
+		{
 			name: "cmd with short flag",
 			c: cli,
 			args: args {
 				input: "withshortflag -f",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with short flags",
+			c: cli,
+			args: args {
+				input: "withshortflags -f -a",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with short combined flags",
+			c: cli,
+			args: args {
+				input: "withshortflags -fa",
 			},
 			wantErr: false,
 		},
@@ -254,6 +637,30 @@ func Test_cli_OneCmd(t *testing.T) {
 			c: cli,
 			args: args {
 				input: "withshortflagandarg -f arg",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with long flag and multiword args",
+			c: cli,
+			args: args {
+				input: "withlongflagandmultiwordargs --flag \"arg0 arg1\" 'arg2 arg3'",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with long flag and multiword args",
+			c: cli,
+			args: args {
+				input: "withshortflagandmultiwordargs -f \"arg0 arg1\" 'arg2 arg3'",
+			},
+			wantErr: false,
+		},
+		{
+			name: "cmd with long flags with multiword args and multiword args",
+			c: cli,
+			args: args {
+				input: "wlfwmaama --flag0=\"flagarg0 flagarg1\" --flag1='flagarg2 flagarg3' \"arg0 arg1\" 'arg2 arg3'",
 			},
 			wantErr: false,
 		},
